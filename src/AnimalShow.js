63708bcc78e3a3a6733e1a240a6422127a29096d
russtack <stirlingest@gmail.com>
2023-03-16 10:14:23 -0400

    initial

---
+++ src/AnimalShow.js
@@ -0,0 +1,27 @@
+import bird from "./svg/bird.svg";
+import cat from "./svg/cat.svg";
+import cow from "./svg/cow.svg";
+import dog from "./svg/dog.svg";
+import gator from "./svg/gator.svg";
+import heart from "./svg/heart.svg";
+import horse from "./svg/horse.svg";
+
+const svgMap = {
+  bird,
+  cat,
+  cow,
+  dog,
+  gator,
+  heart,
+  horse,
+};
+
+function AnimalShow({ type }) {
+  return (
+    <div>
+      <img src={svgMap[type]} alt="" />
+    </div>
+  );
+}
+
+export default AnimalShow;
